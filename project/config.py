467d14423efc97472419bb7ae4a44ecf13e8f59a
Rk <rkramesh@users.noreply.github.com>
2016-11-21 13:56:23 +0530

    adding admin support to config.py

--- project/config.py
+++ project/config.py
@@ -1,6 +1,7 @@
 from cred import *
 import os, logging
 
+
 defaults = {
     "WHATSAPP_LOGIN": phone,
     "WHATSAPP_PW": pw,
@@ -16,6 +17,9 @@ auth = (os.environ.get('WHATSAPP_LOGIN', defaults['WHATSAPP_LOGIN']),
 # Otherwise will leave instantly if added.
 filter_groups = True
 admins = [os.environ.get('WHATSAPP_ADMIN', defaults['WHATSAPP_ADMIN']), ]
+
+#Overall Admin for Autobot,will be used for security and admin features
+botAdmin = '918122753538'
 
 # Bing API for image search
 bing_api_key = os.environ.get('BING_API_KEY', defaults['WHATSAPP_ADMIN'])
@@ -34,3 +38,5 @@ session_db_path = r"C:\Users\radhakrishnanr\Desktop\tmp\sessions.db"
 # If logging_level set to logging.DEBUG, yowsup will log every protocoll message exchange with server.
 log_format = '_%(filename)s_\t[%(levelname)s][%(asctime)-15s] %(message)s'
 logging_level = logging.INFO
+
+
